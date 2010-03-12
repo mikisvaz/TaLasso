c6bf07aaed5ef81c52dcdffe7b99858e8ae3f675
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2010-03-12 16:18:31 +0100

    Change between matrix and list formats

---
+++ lib/matrix_format.rb
@@ -0,0 +1,47 @@
+module MatrixFormat
+  def self.matrix2list(file_matrix, file_row_names, file_col_names, sep=',')
+    row_names  = File.open(file_row_names).read.split("\n")
+    col_names = File.open(file_col_names).read.split("\n")
+
+    list = {}
+    File.open(file_matrix).read.split("\n").each_with_index do |line, i|
+      row_name = row_names[i]
+      values = line.chomp.split(sep)
+      col_names.zip(values) do |p2|
+        col_name, value = p2
+        next if value.to_f == 0
+        list[row_name] ||= {}
+        list[row_name][col_name] =  value
+      end
+    end
+
+    list
+  end
+
+  def self.read_list(file_list, sep="\t")
+    list = {}
+    File.open(file_list).each do |line|
+      row, col, value = line.chomp.split("\t")
+      list[row] = {}
+      list[row][col] = value || 1
+    end
+    list 
+  end
+
+  def self.list2matrix(list, file_matrix, file_row_names, file_col_names, sep =",")
+    row_names = list.keys
+    col_names = list.collect {|key, values| values.keys }.flatten.uniq
+
+
+    File.open(file_row_names, 'w') do |f| f.puts row_names * "\n" end
+    File.open(file_col_names, 'w') do |f| f.puts col_names * "\n" end
+    File.open(file_matrix, 'w') do |f| 
+      f.puts row_names.collect {|row_name|
+        info = list[row_names] || {}
+        col_names.collect { |col_name|
+          info[col_name] || 0
+        } * sep
+      } * "\n"
+    end
+  end
+end
